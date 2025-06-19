00503e7d23859ed43f6e98edd4148e630ad9cf31
hui <huixiankuang4@gmail.com>
2025-06-19 09:12:52 -0400

    add images to showcase

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 Given a URL that includes a table with x and y coordinators and characters, I will print them out for you.
 
 [Example URL](https://docs.google.com/document/d/e/2PACX-1vQGUck9HIFCyezsrBSnmENk5ieJuYwpt7YHYEzeNJkIb9OSDdx-ov2nRNReKQyey-cwJOoEKUhLmN9z/pub)
-
+Table Format:
+![table](table.png)
 Output: 
-![Alt text](output.png)
+![output](output.png)
--- print_2d_grid.py
+++ print_2d_grid.py
@@ -44,6 +44,5 @@ def extract_google_doc_content(url):
         print(''.join(row))
 
 
-
-url = "https://docs.google.com/document/d/e/2PACX-1vQGUck9HIFCyezsrBSnmENk5ieJuYwpt7YHYEzeNJkIb9OSDdx-ov2nRNReKQyey-cwJOoEKUhLmN9z/pub"
+url = "https://docs.google.com/document/d/e/2PACX-1vRM90lh5Zdl8i2RnPAyV9wVgnMP2QfGllWcDmkqzFQrvQjHVZ0WyeiAgPrMZcA5EBcRX_v1dlteJzv6/pub"
 extract_google_doc_content(url)
